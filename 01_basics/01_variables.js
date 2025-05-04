55417b9ed9e1cb1ffd1e2d357997ecf7e7b2fd16
Abu0786 <98095482+Abu0786@users.noreply.github.com>
2025-05-04 04:48:50 +0000

    basic java script

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId=14665
+let accountEmail="abu9145@gmail.com"
+var password="Abc0786"
+/*
+Prefer not to use because of issue in block scope and functional scope
+*/
+accountCity="Patna"
+//accountId=146 //Not Allowed
+accountEmail="abu62015@gmail.com"
+password="Abc0123"
+accountCity="Pune"
+let accountState;
+console.log(accountId);
+console.table([accountId,accountEmail,accountCity,password,accountState]);
+
+
+
